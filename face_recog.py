1a5f21371001879207019bf72554f0242e7f74be
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-15 10:47:55 +0530

    finally solved all installation errors time to start this app

---
+++ face_recog.py
@@ -0,0 +1,14 @@
+import cv2
+import face_recognition
+
+img = cv2.imread('messi.webp')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding = face_recognition.face_encodings(rgb)[0]
+
+img2 = cv2.imread('images/elon musk.webp')
+rgb2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_encoding2 = face_recognition.face_encodings(rgb2)[0]
+
+cv2.imshow('Img', img)
+cv2.imshow('Img 2', img2)
+cv2.waitKey(0)
